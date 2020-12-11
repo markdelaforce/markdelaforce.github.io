21de95fdd8cc5cc87b7c7d7eec470aba4533e69a
Mark Delaforce <inglesconmark@gmail.com>
2020-12-11 11:18:22 +0100

    added 'new' sticker for newly added games

--- index.js
+++ index.js
@@ -8,16 +8,31 @@ inUseItems.forEach(item => {
 	child = item.children[0],
 	size_1 = child.classList[1].substring(1),
 	size_2 = (Number (size_1) +2).toString();
+	if (item.childElementCount === 2) {
+		var newSticker = item.children[1];
+	}
 	item.addEventListener('mouseover', () => {
 		child.style.height = `${size_2}%`;
 		child.style.width = `${size_2}%`;
 		child.style.opacity = '1';
+		if (item.childElementCount === 2) {
+			newSticker.style.height = '41%';
+			newSticker.style.top = '2%';
+			newSticker.style.left = '2%';
+			newSticker.style.opacity = '1';
+		}
 		
 	});
 	item.addEventListener('mouseout', () => {
 		child.style.height = `${size_1}%`;
 		child.style.width = `${size_1}%`;
 		child.style.opacity = '0.9';
+		if (item.childElementCount === 2) {
+			newSticker.style.height = '40%';
+			newSticker.style.top = '2.5%';
+			newSticker.style.left = '2.5%';
+			newSticker.style.opacity = '0.9';
+		}
 	});
 });
 
